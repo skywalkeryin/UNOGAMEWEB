af63d1e7d8c86e790f495ccb82f8f2f630bc4309
skywalkeryin <skywalkeryin007@gmail.com>
2016-08-10 01:09:02 +0800

    create game done

--- UNOGAMEWEB/UNOGAME WEB1/src/java/business/GameManager.java
+++ UNOGAMEWEB/UNOGAME WEB1/src/java/business/GameManager.java
@@ -1,9 +1,14 @@
 
 package business;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import model.UNOgame;
 import javax.ejb.Stateless;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.UUID;
 import javax.json.JsonObject;
@@ -15,18 +20,25 @@ import javax.persistence.PersistenceContext;
 @Stateless
 public class GameManager {
     
-    @PersistenceContext private EntityManager em;
-    private Map<String ,JsonObject> globalgame=new HashMap<>();
+   // @PersistenceContext private EntityManager em;
+    private ArrayList<UNOgame> globalgame=new ArrayList<UNOgame>();
+       
+      
     
-    public Map<String ,JsonObject> creategame( String name){
+    public ArrayList<UNOgame> creategame( String name){
           String gid = UUID.randomUUID().toString().substring(0, 8);
           UNOgame game=new UNOgame();
           game.setGameID(gid);
           game.setGameName(name);
           game.setGameStatus("wating");
           //em.persist(game);
-          globalgame.put(gid, game.toJson());
+          globalgame.add(game);
           return globalgame;
+        // return game;
+    }
+    public ArrayList<UNOgame> getallgames(){
+         return globalgame;
+        
     }
           
        
